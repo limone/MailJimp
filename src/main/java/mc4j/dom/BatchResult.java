d04df5a057819368bb9a93e440b123f981b01096
Michael Laccetti <michael@laccetti.com>
2011-05-03 11:23:09 -0400

    * Flipping to the LGPL * Updated to Spring 3.0.5 * Added versions to Maven plugins to make Maven stop complaining

--- src/main/java/mc4j/dom/BatchResult.java
+++ src/main/java/mc4j/dom/BatchResult.java
@@ -1,20 +1,22 @@
-package mc4j.dom;
 /*
  * Copyright 2011 Eike Hirsch
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * 
+ * This file is part of MailChimp4J.
+ * 
+ * MailChimp4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * MailChimp4J is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MailChimp4J.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+package mc4j.dom;
 
 /**
  * Represents the response to an batch operation like {@link mc4j.service.IMailChimpService#listBatchSubscribe(String, Object[], boolean, boolean, boolean)}.
--- src/test/java/mc4j/service/impl/TestMailChimpParser.java
+++ src/test/java/mc4j/service/impl/TestMailChimpParser.java
@@ -1,20 +1,21 @@
-package mc4j.service.impl;
 /*
  * Copyright 2011 Eike Hirsch
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * 
+ * This file is part of MailChimp4J.
+ * 
+ * MailChimp4J is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * MailChimp4J is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MailChimp4J.  If not, see <http://www.gnu.org/licenses/>.
  */
+package mc4j.service.impl;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.collection.IsMapContaining.*;
@@ -28,7 +29,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
  * Eike Hirsch (me at eike-hirsch dot net)
  * Date: 27.04.11
  * Time: 06:38
