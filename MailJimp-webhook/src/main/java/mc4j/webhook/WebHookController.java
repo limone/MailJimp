63b9ef8b338a3708727cb9397abc4873f1b84525
Eike Hirsch <me@localhorst-3.local>
2011-05-05 22:57:27 +0200

    First working webhook callback implemented. The rest should be easy.

--- MailJimp-webhook/src/main/java/mc4j/webhook/WebHookController.java
+++ MailJimp-webhook/src/main/java/mc4j/webhook/WebHookController.java
@@ -1,20 +1,32 @@
+/*
+ * Copyright 2011 Eike Hirsch
+ *
+ * This file is part of MailJimp.
+ *
+ * MailJimp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * MailJimp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MailJimp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package mc4j.webhook;
 
-import mc4j.dom.MemberInfo;
+import mc4j.service.MailChimpException;
 import mc4j.service.impl.MailChimpParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,11 +41,6 @@ import java.util.regex.Pattern;
 @Controller
 public class WebHookController {
 
-	// TODO: move to core
-	public static class Group {
-	}
-
-
 	private static Pattern DATA_PATTERN = Pattern.compile("data\\[(\\w+)\\](\\[(\\w+)\\](\\[(\\d+)\\]\\[(\\w+)\\])?)?");
 	private static int INDEX_PARAM_NAME = 1;
 	private static int INDEX_MERGES_NAME = 3;
@@ -43,54 +50,71 @@ public class WebHookController {
 	@Autowired
 	private IWebHookAdapter webHookAdapter;
 
-	// TODO: refactor the used methods out of this class
+	// as our core module can be run without spring in place, we can not auto wire the parser in.
 	private MailChimpParser parser = new MailChimpParser();
 
 
 	@RequestMapping(params = "type=subscribe")
-	public @ResponseBody String subscribe(WebRequest request) {
-		Map<String,String[]> parameterMap = request.getParameterMap();
-		Map<String,Object> convertible = new HashMap<String, Object>();
-		Map<String,Object> merges = new HashMap<String, Object>();
-		convertible.put("merges", merges);
-
-		for (String key : parameterMap.keySet()) {
-			Matcher matcher = DATA_PATTERN.matcher(key);
-			if(matcher.matches()) {
-				final String value = parameterMap.get(key)[0];
-				if( null == matcher.group(INDEX_MERGES_NAME)) {
-					// simple values
-					convertible.put(matcher.group(INDEX_PARAM_NAME), value);
-				} else if( null == matcher.group(INDEX_GROUP_INDEX)) {
-					// simple merges
-					merges.put(matcher.group(INDEX_MERGES_NAME), value );
-				} else {
-					// groupings
-
-				}
-			}
-		}
-		debug(convertible);
-		webHookAdapter.userSubscribed(null);
+	public @ResponseBody String subscribe(WebRequest request) throws MailChimpException {
+		webHookAdapter.userSubscribed(parser.parseListMemberInfo(parseRequest(request)));
 		return "Copy that!";
 	}
 
-	private void debug(Map<String, Object> convertible) {
-		for( String key : convertible.keySet() ) {
-			final Object value = convertible.get(key);
-			if( Map.class.isAssignableFrom(value.getClass())) {
-				debug((Map<String, Object>) value);
-			} else {
-				System.out.format("\n%s\t%s", key, value);
+	// Watch out! This is going to be dirty! You have been warned.
+	private Map<String, Object> parseRequest(WebRequest request) {
+		Map<String,String[]> parameterMap = request.getParameterMap();
+		Map<String,Object> convertible = new HashMap<String, Object>();
+
+		for (String parameterKey : parameterMap.keySet()) {
+			Matcher matcher = DATA_PATTERN.matcher(parameterKey);
+			if(matcher.matches()) {
+				String key = matcher.group(INDEX_PARAM_NAME);
+				Object value = parameterMap.get(parameterKey)[0];
+
+				if( null == matcher.group(INDEX_MERGES_NAME)) {
+					// simple values
+					convertible.put(key, value);
+				} else if( null == matcher.group(INDEX_GROUP_INDEX)) {
+					// mapped values
+					Map<String, Object> map = getMapProperty(convertible, key);
+					map.put(matcher.group(INDEX_MERGES_NAME), value );
+				} else {
+					// merge values in an array
+					// If you like good coding don't read on!
+					Map<String, Object> map = getMapProperty(convertible, key);
+					// now check for the array
+					String arrayKey = matcher.group(INDEX_MERGES_NAME);
+					int arrayIndex = Integer.valueOf( matcher.group(INDEX_GROUP_INDEX));
+					if( null == map.get(arrayKey)) {
+						map.put(arrayKey, new Object[]{});
+					}
+					Object[] array = (Object[]) map.get(arrayKey);
+					while( array.length <= arrayIndex ) {
+						Object[] newArray = new Object[array.length+1];
+						System.arraycopy(array, 0, newArray, 0, array.length);
+						array = newArray;
+					}
+					if( null == array[arrayIndex]) {
+						array[arrayIndex] = new HashMap<String,Object>();
+					}
+					map.put(arrayKey, array);
+					//noinspection unchecked
+					((Map<String, Object>) array[arrayIndex]).put(matcher.group(INDEX_GROUP_PARAM), value);
+				}
+
 			}
 		}
-		System.out.print('\n');
+		return convertible;
 	}
 
-	private void bindMemberInfo(MemberInfo memberInfo, Matcher matcher, String value) {
-
-
+	@SuppressWarnings({"unchecked"})
+	private Map<String, Object> getMapProperty(Map<String, Object> convertible, String key) {
+		if( !convertible.containsKey(key)) {
+			convertible.put(key, new HashMap<String, Object>());
+		}
+		return (Map<String, Object>) convertible.get(key);
 	}
+
 
 
 }
--- MailJimp-webhook/src/test/java/mc4j/webhook/MyTestWebHookAdapter.java
+++ MailJimp-webhook/src/test/java/mc4j/webhook/MyTestWebHookAdapter.java
@@ -1,11 +1,33 @@
+/*
+ * Copyright 2011 Eike Hirsch
+ *
+ * This file is part of MailJimp.
+ *
+ * MailJimp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * MailJimp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MailJimp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package mc4j.webhook;
 
-import mc4j.dom.MemberInfo;
+import mc4j.dom.list.MemberInfo;
+import mc4j.service.impl.MailChimpConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Author: Eike Hirsch (me at eike-hirsch dot net)
@@ -25,6 +47,10 @@ public class MyTestWebHookAdapter implements IWebHookAdapter {
 
 	@Override
 	public void userSubscribed(MemberInfo memberInfo) {
-		LOG.info("A new member of the jungle.");
+		assertNotNull(memberInfo);
+
+		assertEquals("me@eike-hirsch.net", memberInfo.getEmail());
+		assertEquals("Eike", memberInfo.getMerges().get(MailChimpConstants.FNAME));
+		assertEquals("VALUE1, VALUE2", memberInfo.getGroupings()[0].getGroups());
 	}
 }
