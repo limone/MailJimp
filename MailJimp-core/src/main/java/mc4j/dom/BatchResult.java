ba002cb99a1b0c93bd873f7402a74193487552ad
Eike Hirsch <me@nothing.(none)>
2011-05-05 17:09:45 +0200

    * replaced MailChimp4J with MailJimp in the copyrights * introduced new dom packages (list, security)

--- MailJimp-core/src/main/java/mc4j/dom/BatchResult.java
+++ MailJimp-core/src/main/java/mc4j/dom/BatchResult.java
@@ -1,19 +1,19 @@
 /*
  * Copyright 2011 Eike Hirsch
  * 
- * This file is part of MailChimp4J.
+ * This file is part of MailJimp.
  * 
- * MailChimp4J is free software: you can redistribute it and/or modify
+ * MailJimp is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, version 3 of the License.
  * 
- * MailChimp4J is distributed in the hope that it will be useful,
+ * MailJimp is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with MailChimp4J.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MailJimp.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package mc4j.dom;
