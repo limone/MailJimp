eccc27871bc819b9308162780e6ee8153a1311c1
Eike Hirsch <me@nothing.(none)>
2011-05-05 17:07:01 +0200

    * Made MailChimpConstants an interface as it's more natural for this kind of class. * replaced MailChimp4J with MailJimp in the copyrights

--- MailJimp-core/src/main/java/mc4j/service/impl/MailChimpConstants.java
+++ MailJimp-core/src/main/java/mc4j/service/impl/MailChimpConstants.java
@@ -1,24 +1,24 @@
 /*
  * Copyright 2010-2011 Michael Laccetti
  * 
- * This file is part of MailChimp4J.
+ * This file is part of MailJimp.
  * 
- * MailChimp4J is free software: you can redistribute it and/or modify
+ * MailJimp is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, version 3 of the License.
  * 
- * MailChimp4J is distributed in the hope that it will be useful,
+ * MailJimp is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with MailChimp4J.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MailJimp.  If not, see <http://www.gnu.org/licenses/>.
  */
 package mc4j.service.impl;
 
 import java.text.SimpleDateFormat;
 
-public class MailChimpConstants {
-	static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+public interface MailChimpConstants {
+	SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 }
